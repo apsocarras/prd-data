ce3bcd72368f1510e9988eda62dbbb5d2d21495a
Alexander Socarras <alex@Alexanders-Mac-mini.local>
2025-01-07 13:18:35 -0500

    rename to prd_data

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
-from setuptools import setup
+import os
+import zipfile
 
-from setuptools import find_packages
+from setuptools import find_packages, setup
 from setuptools.command.build_py import build_py
 
-import os 
-import zipfile 
+PACKAGE_NAME = "prd_data"
 
-from enum import Enum
-
-PACKAGE_NAME = "prd-data" 
 
 class my_build_py(build_py):
     """Custom build step to unzip packaged parquets."""
-    def run(self) -> None: 
 
-        src_dir = os.path.abspath(os.path.join('src', PACKAGE_NAME))
-        zip_path = os.path.join(src_dir, 'data.parquet.zip') 
+    def run(self) -> None:
+        src_dir = os.path.abspath(os.path.join("src", PACKAGE_NAME))
+        zip_path = os.path.join(src_dir, "data.parquet.zip")
 
         if not os.path.exists(zip_path):
             error_msg = f"Missing '{zip_path}. Ensure package was built correctly."
@@ -23,21 +20,22 @@ class my_build_py(build_py):
 
         extract_to = src_dir
         # os.makedirs(extract_to, exist_ok=True)
-        
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref: 
+
+        with zipfile.ZipFile(zip_path, "r") as zip_ref:
             zip_ref.extractall(extract_to)
-        
+
         super().run()
+
 
 setup(
     name=PACKAGE_NAME,
     version="0.1.0",
     package_dir={"": "src"},
     packages=find_packages(where="src", exclude=("test*", "testing*", "tests*")),
-    cmdclass={'build_py':my_build_py}, 
+    cmdclass={"build_py": my_build_py},
     include_package_data=True,
     package_data={
-        "prd-data": [
+        "prd_data": [
             "*.zip",
         ]
     },
